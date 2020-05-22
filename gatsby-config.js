1c4d5f60ddcd9d93ba98d7b346c9b56391c503f1
Zenious <7553066+Zenious@users.noreply.github.com>
2020-05-23 03:57:47 +0800

    Revamp site (again)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Zheng Wei`,
+    description: `Personal Website`,
+    author: `@zenious`,
   },
+  pathPrefix: "/portfolio-site",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
@@ -18,13 +19,19 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Zheng Wei's Site`,
+        short_name: `Zheng Wei`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#50514F`,
+        theme_color: `#50514F`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo-icon-squared.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/pages/index.tsx
@@ -0,0 +1,102 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+
+import SEO from "../components/seo"
+
+import Logo from "../images/logo-inverted.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPython } from '@fortawesome/free-brands-svg-icons'
+import { faJava } from '@fortawesome/free-brands-svg-icons'
+import { faNodeJs } from '@fortawesome/free-brands-svg-icons'
+import { faAws } from '@fortawesome/free-brands-svg-icons'
+
+const IndexPage = () => (
+<>
+  <SEO title="Hello World"/>
+  <body style={{
+    'backgroundColor': '#50514F',
+    'padding': '0.25rem'
+  }}>
+  <div style={{
+    'width': '60%',
+    'margin': '0 auto',
+    'color' : 'white',
+
+  }}>
+  <h1 style={{
+    'padding': '1rem',
+    'textAlign': 'center'
+  }}>
+    <img src={Logo}></img>
+    <br/>
+  HELLO WORLD
+  </h1>
+  <hr></hr>
+  
+  <h2>Hi There</h2>
+  <p>I'm Zheng Wei, an undergraduate student studying Computer Science in NUS, Singapore. <br/>I have strong interest in DevOps & Security related topics. Feel free to contact me!</p>
+  <hr/>
+  <h2>Education</h2>
+  <p>National University of Singapore <br/> 2017-2021<br/> B.Comp in Computer Science</p>
+  <p>Concordia University <br/> Jan'19 - May'19 <br/> Oversea Winter Student Exchange Programme</p>
+  <p>Singapore Management of Singapore <br/> Aug'19 - Dec'19 <br/> Singapore University Student Exchange Programme (Partial) </p>
+  <p>Singapore Polytechnic <br/> 2012-2015 <br/> Dip in Infocomm. Security Management (Merit)</p>
+  <hr/>
+  <h2>Work Experience</h2>
+  <p>* For most updated information, refer to LinkedIn</p>
+  <p>NodeFlair <br/> Oct'19 - Dec'19 <br/> Software Engineering Intern</p>
+  <p>National University of Singapore <br/> Aug'19 - Dec'19 <br/> Undergraduate Teaching Assistant</p>
+  <p>M1<br/> May'19 - Aug'19<br/> Software Development Intern</p>
+  <p>Jewel Paymentech<br/> Oct'14 - Sep'15 <br/> Associate Developer</p>
+  <hr/>
+  <h2>Tech Languages Known</h2>
+  <p>Proficient: {" "}
+  <FontAwesomeIcon icon={faPython} /> {" "}
+  <FontAwesomeIcon icon={faJava} /> {" "}
+  <FontAwesomeIcon icon={faNodeJs} /> {" "}
+  </p>
+  <p> Good to get by:
+  C++
+
+  </p>
+  <hr/>
+  <h2>Projects</h2>
+  <p>UnderstandNot: Your Video-Captioning Solution <br/>Python Web App thats provides a platform for students to obtain subtitles for videos and edit the respective subtitles accordingly. It deploy AWS Transcribe as the main engine to caption the video. The application was then hosted with AWS EC2.<br/>
+  Tech Stack: {" "}
+  <FontAwesomeIcon icon={faPython} /> {" "}
+  <FontAwesomeIcon icon={faAws} /> {" "}
+</p>
+  <p>IDA-SAI: A Framework for Search Based Malware Analysis <br/>Product of Final Year Project in Singapore Polytechnic in corporation with DSO. Plugin to simplify the process and introduce alternative way to perform static analysis on malware.<br/> 
+  Tech Stack: {" "}
+  <FontAwesomeIcon icon={faPython} /> {" "}
+  </p>
+  <hr/>
+  <h2>Media Spotlight</h2>
+  <p><a href="https://www.comp.nus.edu.sg/news/3137-2019-bug-bounty/">NUS Bug Bounty Challenge 2019</a></p>
+  <hr/>
+  <h2>Contact Me</h2>
+  <p>
+    <li><a href="https://www.linkedin.com/in/koh-zheng-wei/">Linkedin</a></li>
+    <li><a href="mailto: contact@kohzw.com">E-Mail</a></li>
+    <li><a href="https://github.com/zenious">Github</a></li></p>
+  <hr/>
+  </div>
+  </body>
+  <footer style={{
+    'backgroundColor': '#50514F',
+      'margin': '0 auto',
+      'color' : 'white',
+  }}>
+    <div style={{
+      'width': '60%',
+      'margin': '0 auto',
+      'textAlign': 'right'
+    }}>   © {new Date().getFullYear()}, Built with
+    {` `}
+    <a href="https://www.gatsbyjs.org">Gatsby</a>
+ </div>
+ </footer>
+ </>
+)
+
+export default IndexPage
