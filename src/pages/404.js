968b2f59a0b94ab2b8d319c3d0519079d173b0c6
Zenious <7553066+Zenious@users.noreply.github.com>
2020-05-24 03:06:09 +0800

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,27 @@
+import React from "react"
+import Status404Logo from "../images/404-not-found.png"
+
 
 const NotFoundPage = () => (
- "404"
+    <div style={{
+        'backgroundColor': '#50514F',
+        'height': '100vh',
+    }}>
+        <div style={{
+            'margin': '0 auto',
+            'textAlign': 'center',
+            'verticalAlign': 'middle',
+            'width': '90%',
+            'padding': '30px',
+            'color': '#ececec'
+        }}>
+        <h1>Opps... You reached page that yet to exist</h1>
+        <img src={Status404Logo} />
+        <h2>Return to <a href="/" style={{
+        'color' : '#ececec',
+        }}>Homepage </a></h2>
+    </div>
+    </div>
 )
 
 export default NotFoundPage
