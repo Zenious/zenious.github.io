6d0f535142b8aa8e353cfc9fd90441347e7f3405
Zenious <7553066+Zenious@users.noreply.github.com>
2020-05-25 04:40:36 +0800

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-167540219-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
